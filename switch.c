4676737143acb6d0fea2fcc666403597bfb39461
Monika G <monikaggopalappa@gmail.com>
2025-03-26 13:45:46 +0530

    case3

--- switch.c
+++ switch.c
@@ -2,12 +2,15 @@
 void main() {
 	switch {
 		case '1':
-			printf("b.tech CSE);
+			printf("b.tech CSE");
 			break;
 	switch {
 		case '2':
-			printf("b.tech AIML);
+			printf("b.tech AIML");
+			break;
+	switch {
+		case '3':
+			printf{"b.tech ECE");
 			break;
 
-		
 
